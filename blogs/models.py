6a0706519ed914b209eded46670ed7c06921673e
hkassow <hunterkassow@protonmail.com>
2022-12-15 19:31:06 -0600

    created email subscriber class and began prep for auto sending emails once a post is created

--- blogs/models.py
+++ blogs/models.py
@@ -1,13 +1,31 @@
 from django.db import models
 from tinymce.models import HTMLField
+from django.utils import timezone
 # Create your models here.
 
 
 class Post(models.Model):
     post_title = models.CharField(max_length=30)
     pub_date = models.DateTimeField('date published')
-    update_date = models.DateTimeField('last updated', auto_now=True)
+    update_date = models.DateTimeField('last updated')
     post_body = HTMLField()
 
     def __str__(self) -> str:
         return self.post_title
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            pub_date = timezone.now()
+        self.update_date = timezone.now()
+        super().save(*args, **kwargs)  # Call the "real" save() method.
+        print('A post has been created or updated and this is its id:', self.id)
+
+class Subscribers(models.Model):
+    email = models.CharField(max_length=60)
+    sub_date = models.DateTimeField('date subscribed')
+
+    def save(self, *args, **kwargs):
+        self.sub_date = timezone.now()
+        super().save(*args, **kwargs)  # Call the "real" save() method.
+        
+
